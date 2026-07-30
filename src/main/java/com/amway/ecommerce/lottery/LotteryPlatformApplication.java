c98dd1c151e50ec0950ec4f3c08c1d5805919150
Charles <btp_charles@photons.com.tw>
2026-07-30 12:16:40 +0800

    chore: 初始化 spring boot 專案 + maven wrapper

---
+++ src/main/java/com/amway/ecommerce/lottery/LotteryPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.amway.ecommerce.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryPlatformApplication.class, args);
+    }
+}
